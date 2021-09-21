dbe05a4d222a34ebf8e4c4dbb116a4b91a1dd232
j-void <janmeshukey@gmail.com>
2021-09-21 20:09:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,13 +17,13 @@ num_workers = 3
 batch_size=1
 # ProGAN specific parameters
 LEARNING_RATE = 1e-3
-BATCH_SIZES = [32, 32, 32, 16, 16, 16, 16, 8]
+BATCH_SIZES = [32, 32, 32, 16, 8, 4, 2, 1]
 CHANNELS_IMG = 3
 Z_DIM = 512  # should be 512 in original paper
 IN_CHANNELS = 512  # should be 512 in original paper
 CRITIC_ITERATIONS = 1
 LAMBDA_GP = 10
-PROGRESSIVE_EPOCHS = [30] * len(BATCH_SIZES)
+PROGRESSIVE_EPOCHS = [30, 40, 50, 60, 70, 80, 90, 100]#[30] * len(BATCH_SIZES)
 FIXED_NOISE = torch.randn(8, Z_DIM, 1, 2).to(device)
 START_TRAIN_AT_IMG_SIZE = 4
 LOAD_MODEL = False
