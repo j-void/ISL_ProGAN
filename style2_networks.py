7da9b70bd966e05809537072b6ff2c244dea47b8
j-void <janmeshukey@gmail.com>
2021-10-23 20:07:19 +0530

    Update style2_networks.py

--- style2_networks.py
+++ style2_networks.py
@@ -320,7 +320,7 @@ class ConstantInput(nn.Module):
     def __init__(self, channel, size=4):
         super().__init__()
 
-        self.input = nn.Parameter(torch.randn(1, channel, size, size*2))
+        self.input = nn.Parameter(torch.randn(1, channel, size, size))
 
     def forward(self, input):
         batch = input.shape[0]
